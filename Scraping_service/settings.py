7c659c4b69f98cc26de24ff2d9cbaffdb23a3bf6
KLYMENKORUS <klimenkoruslan11@gmail.com>
2022-12-08 16:30:51 +0200

    add app accounts

--- Scraping_service/settings.py
+++ Scraping_service/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # My Apps
     'scraping.apps.ScrapingConfig',
+    'accounts.apps.AccountsConfig',
 
 ]
 
@@ -118,3 +119,5 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.MyUser'
