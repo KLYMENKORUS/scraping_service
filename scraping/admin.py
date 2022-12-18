2d58ffdd69a8d62ddb6aa87504d6e7e30b510a8c
KLYMENKORUS <klimenkoruslan11@gmail.com>
2022-12-18 15:22:41 +0200

    update admin

--- scraping/admin.py
+++ scraping/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import City, Language, Vacancy
+from .models import City, Language, Vacancy, Error
 
 
 admin.site.register(City)
 admin.site.register(Language)
 admin.site.register(Vacancy)
+admin.site.register(Error)
 
 
