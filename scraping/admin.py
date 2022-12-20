01d4689b9dc4823a82af80f16d24ebeef57b7fe9
KLYMENKORUS <klimenkoruslan11@gmail.com>
2022-12-20 13:53:05 +0200

    update admin.py (accounts, scraping)

--- scraping/admin.py
+++ scraping/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import City, Language, Vacancy, Error
+from .models import City, Language, Vacancy, Error, Url
+
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_filter = ['city', 'language']
 
 
 admin.site.register(City)
 admin.site.register(Language)
-admin.site.register(Vacancy)
 admin.site.register(Error)
+admin.site.register(Url)
 
 
