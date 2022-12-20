0092992e6c2316dc1394e733a8e87b2f4d32bdce
KLYMENKORUS <klimenkoruslan11@gmail.com>
2022-12-20 13:50:31 +0200

    update run_scraping.py

--- run_scraping.py
+++ run_scraping.py
@@ -1,6 +1,7 @@
-import json
+import asyncio
 import os, sys
 
+from django.contrib.auth import get_user_model
 
 project = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(project)
@@ -12,26 +13,68 @@ django.setup()
 
 from django.db import DatabaseError
 from scraping.parser.parsers import *
-from scraping.models import Vacancy, City, Language, Error
+from scraping.models import Vacancy, Error, Url
 
+User = get_user_model()
 
 parsers = (
-    (get_work_ua, 'https://www.work.ua/jobs-kyiv-python/'),
-    (get_dou_ua, 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D1%97%D0%B2&category=Python'),
-    (djinni_co, 'https://djinni.co/jobs/?location=kyiv&region=UKR&primary_keyword=Python')
+    (get_work_ua, 'get_work_ua'),
+    (get_dou_ua, 'get_dou_ua'),
+    (djinni_co, 'djinni_co')
 )
 
-city = City.objects.filter(slug='kiev').first()
-language = Language.objects.filter(slug='python').first()
 jobs, errors = [], []
 
-for func, url in parsers:
-    j, e = func(url)
-    jobs += j
-    errors += e
+
+def get_settings():
+    qs = User.objects.filter(send_email=True).values()
+    settings_lst = set((q['city_id'], q['language_id']) for q in qs)
+    return settings_lst
+
+
+def get_urls(_settings):
+    qs = Url.objects.all().values()
+    url_dict = {(q['city_id'], q['language_id']): q['url_data'] for q in qs}
+    urls = []
+    for pair in _settings:
+        if pair in url_dict:
+            tmp = {}
+            tmp['city'] = pair[0]
+            tmp['language'] = pair[1]
+            url_data = url_dict.get(pair)
+            if url_data:
+                tmp['url_data'] = url_dict.get(pair)
+                urls.append(tmp)
+    return urls
+
+
+async def main(value):
+    func, url, city, language = value
+    job, err, = await loop.run_in_executor(None, func, url, city, language)
+    jobs.extend(job)
+    errors.extend(err)
+
+settings = get_settings()
+url_list = get_urls(settings)
+
+loop = asyncio.get_event_loop()
+tmp_tasks = [(func, data['url_data'][key], data['city'], data['language'])
+             for data in url_list for func, key in parsers]
+
+if tmp_tasks:
+    tasks = asyncio.wait([loop.create_task(main(f)) for f in tmp_tasks])
+    loop.run_until_complete(tasks)
+    loop.close()
+
+# for data in url_list:
+#     for func, key in parsers:
+#         url = data['url_data'][key]
+#         j, e = func(url, city=data['city'], language=data['language'])
+#         jobs += j
+#         errors += e
 
 for job in jobs:
-    v = Vacancy(**job, city=city, language=language)
+    v = Vacancy(**job)
     try:
         v.save()
     except DatabaseError:
@@ -39,6 +82,6 @@ for job in jobs:
 
 if errors:
     er = Error(data=errors).save()
-
-# with open('jobs.txt', 'a', encoding='utf-8') as file:
-#     json.dump(jobs, file, indent=4, ensure_ascii=False)
+#
+# with open('file.txt', 'w', encoding='utf-8') as file:
+#     [file.write(f'{line}\n') for line in jobs]
