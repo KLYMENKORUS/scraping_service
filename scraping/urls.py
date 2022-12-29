5e669e4e622813bae2efc0a8700f584d62588156
KLYMENKORUS <klimenkoruslan11@gmail.com>
2022-12-29 23:59:04 +0200

    update service

--- scraping/urls.py
+++ scraping/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'scraping'
 
 urlpatterns = [
-    path('list/vacancy/', views.list_view, name='list_view'),
+    path('list/vacancy/', views.VacancyList.as_view(), name='list_view'),
+    path('detail/<int:pk>/vacancy/', views.VacancyDetail.as_view(), name='detail'),
     path('home/', views.home, name='home'),
 ]
--- scraping/views.py
+++ scraping/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 from .models import Vacancy
 from .forms import FindForm
 
@@ -32,3 +33,38 @@ def list_view(request):
         context['object_list'] = page_obj
 
     return render(request, 'scraping/list.html', context)
+
+
+class VacancyDetail(DetailView):
+    queryset = Vacancy.objects.all()
+    template_name = 'scraping/detail.html'
+    context_object_name = 'vacancy_detail'
+
+
+class VacancyList(ListView):
+    model = Vacancy
+    template_name = 'scraping/list.html'
+    form = FindForm()
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['city'] = self.request.GET.get('city')
+        context['language'] = self.request.GET.get('language')
+        context['form'] = self.form
+        return context
+
+    def get_queryset(self):
+        city = self.request.GET.get('city')
+        language = self.request.GET.get('language')
+        qs = []
+        if city or language:
+            _filter = {}
+            if city:
+                _filter['city__slug'] = city
+            if language:
+                _filter['language__slug'] = language
+
+            qs = Vacancy.objects.filter(**_filter)
+        return qs
+
